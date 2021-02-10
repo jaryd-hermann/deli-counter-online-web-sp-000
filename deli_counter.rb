e4e23a8a773490ffb94efc984d0c3593bc675d9c
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-10 12:29:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,19 @@
 # Write your code here.
 
-def line(array)
-  if array.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    array.each_with_index do |value, index|
+    katz_deli.each_with_index do |value, index|
       message += " #{index.to_i+1}. #{value}"
     end
     puts "#{message}"
   end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line." 
 end
