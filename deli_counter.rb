7f53320218679db16f45f9bdca656b02a5fd93dd
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-10 12:19:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here.
 
 def line(array)
-  if array.length == 0
-    puts "The line is currently empty."
+  if array.length > 0
+    message = "The line is currently:"
+    array.each_with_index do |value, index|
+      message += "#{index.to_i+1}. #{value}" 
+    end
+    puts "#{message}"
   else
-    puts "You are number #{array.lenght} in line"
+      puts "The line is currently empty."
   end
 end
