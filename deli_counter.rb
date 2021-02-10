de502bfe0c49b891643723565f1f2661153b15c7
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-10 12:19:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   if array.length > 0
     message = "The line is currently:"
     array.each_with_index do |value, index|
-      message += "#{index.to_i+1}. #{value}" 
+      message += "#{index.to_i+1}. #{value}"
     end
     puts "#{message}"
   else
